b7baa7b991a69b5f95bd37cf13068ca8651c4a91
IsraelMerlyn <merlyn142a@gmail.com>
2025-04-25 20:44:42 -0600

    Act6-Creando Metodo PUT en el controlador

--- src/main/java/com/israel/alumnos/controllers/AlumnoController.java
+++ src/main/java/com/israel/alumnos/controllers/AlumnoController.java
@@ -3,13 +3,16 @@ package com.israel.alumnos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.israel.alumnos.model.Alumno;
 import com.israel.alumnos.repository.AlumnoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -30,6 +33,22 @@ public class AlumnoController {
     public Alumno insertarAlumno(@RequestBody Alumno alumno) {
         return alumnoRepository.save(alumno);
 
+    }
+
+    // Metodo para editar un alumno en la base de datos
+    @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<Alumno> actualizarAlumno(@PathVariable Long id, @RequestBody Alumno alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellido(alumno.getApellido());
+            alumnoExistente.setEmail(alumno.getEmail());
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setImagenURL(alumno.getImagenURL());
+            Alumno actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 
 }
